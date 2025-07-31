dd32bc24b3c2e23aa2174141c219fdd2412077b4
anikaBushra81 <bushraanika64@gmail.com>
2025-07-31 16:30:39 +0600

    9 commit

---
+++ Python/accessTuples.py
@@ -0,0 +1,26 @@
+# access tuple items
+tuple1=("lily","sunflower")
+print(tuple1[1])
+
+# negative indexing
+tuple2=("akhi","lamia","sathi")
+print(tuple2[-2])
+print("\n")
+
+#range of indexing
+tuple3=("dog","horse","bird","butterfly","cat")
+print(tuple3[1:4])
+print("\n")
+print(tuple3[:3])
+print("\n")
+print(tuple3[3:])
+print("\n")
+
+# negstive inexing
+print(tuple3[-3:-1])
+print("\n")
+
+# check if item exists
+tuple4=["apple","mango","banana"]
+if "apple" in tuple4:
+    print("yes")
---
+++ Python/tuple.py
@@ -0,0 +1,19 @@
+# tuples areused to store muliple items in a single variable
+tuple1=("bird","dog","cat")
+print(tuple1)
+
+# allow duplicates
+tuple2=["anika","akhi","lamia","sathi","akhi"]
+print(tuple2)
+
+# tuple length
+tuple3=(1,3,556,9)
+print(len(tuple3))
+
+# create tuple with one item
+# to create a tuple with only one item,you have to add a comma after the item 
+# otherwise python will not recognize it as a tuple
+tuple4=("hi",)
+print(tuple4)
+print(type(tuple4))
+
